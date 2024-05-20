836bc475108be000c5cd19a2b0fb7cb8b1eb9fb6
SriSabarish2021 <142215318+SriSabarish2021@users.noreply.github.com>
2024-05-20 21:45:30 +0530

    Update voice.js

--- voice.js
+++ voice.js
@@ -9,6 +9,13 @@ const contain=document.querySelector(".container")
 contain.appendChild(para)
 
 speechrecongnition.addEventListener('result',element =>{
-console.log(element)
+const map=Array.from(element.results).map(resul=>resul[0]).map(resul=>resul.transcript).join("")
+para.textContent=map
+if(element.results[0].isFinal){
+    para=document.createElement("p")
+    contain.appendChild(para)
+}
 })
+
+speechrecongnition.addEventListener('end',speechrecongnition.start)
 speechrecongnition.start() 
